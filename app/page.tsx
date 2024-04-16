b83476846e261ce4c4896dd22eb90c2b898c674a
khot-aditya <adityakhot6838@gmail.com>
2024-04-16 15:49:13 +0530

    Refactor Home component and update Supabase client files

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { cookies } from 'next/headers'
+import { createClient } from './utils/supabase/server'
 
-const HomePage: React.FC = () => {
+export const runtime = 'edge'
+
+export default async function Home() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('notes').select()
+
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is a simple Next.js home page.</p>
-    </div>
-  );
-};
-
-export default HomePage;
+    <ul className='w-screen h-screen flex items-center justify-center flex-col bg-white text-black'>
+      {todos?.map((todo, index) => (
+        <li key={index}>
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  )
+}
