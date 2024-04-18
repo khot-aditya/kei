f0411c13ed094e3c3be931842701fabdcebb5185
khot-aditya <adityakhot6838@gmail.com>
2024-04-18 16:39:36 +0530

    Refactor Home component and add utility functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,102 @@
-"use client";
-
 import useController from "./controllers/home/useSetupController";
+import { Button } from "@/app/components/ui/button"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/components/ui/alert-dialog"
+import { Avatar, AvatarFallback, AvatarImage } from "@/app/components/ui/avatar"
+import { Checkbox } from "@/app/components/ui/checkbox"
+import {
+  Credenza,
+  CredenzaBody,
+  CredenzaClose,
+  CredenzaContent,
+  CredenzaDescription,
+  CredenzaFooter,
+  CredenzaHeader,
+  CredenzaTitle,
+  CredenzaTrigger,
+} from "@/app/components/ui/credenza"
+import { Label } from "@/app/components/ui/label"
 
 export default function Home() {
 
-  const { todos } = useController();
+  // const { todos } = useController();
 
   return (
-    <div className='w-screen h-screen flex items-center justify-center flex-col bg-white text-black'>
-      <h1 className='text-2xl mb-4'>Todo List</h1>
+    <div
+      // vaul-drawer-wrapper=""
+      className='w-screen h-screen flex items-center gap-4 justify-center flex-col bg-white text-black'>
+      {/* <h1 className='text-2xl mb-4'>Todo List</h1>
       <ul>
         {todos?.map((todo) => (
           <li key={todo.id}>
             {todo.title}
           </li>
         ))}
-      </ul>
+      </ul> */}
+
+      <Credenza>
+        <CredenzaTrigger asChild>
+          <button>Open modal</button>
+        </CredenzaTrigger>
+        <CredenzaContent>
+          <CredenzaHeader>
+            <CredenzaTitle>Credenza</CredenzaTitle>
+            <CredenzaDescription>
+              A responsive modal component for shadcn/ui.
+            </CredenzaDescription>
+          </CredenzaHeader>
+          <CredenzaBody>
+            This component is built using shadcn/ui&apos;s dialog and drawer
+            component, which is built on top of Vaul.
+          </CredenzaBody>
+          <CredenzaFooter>
+            <CredenzaClose asChild>
+              <button>Close</button>
+            </CredenzaClose>
+          </CredenzaFooter>
+        </CredenzaContent>
+      </Credenza>
+
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">Accept terms and conditions</Label>
+      </div>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+
+      <Button>Click me</Button>
+
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
     </div>
   );
 }
