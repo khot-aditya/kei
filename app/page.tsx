14ab841ce5defe890273d00621b45bab1375cdeb
khot-aditya <adityakhot6838@gmail.com>
2024-04-18 22:03:31 +0530

    Update package dependencies and add Textarea component

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ import {
 } from "@/app/components/ui/table"
 import Fab from "./views/fab"
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col relative">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
@@ -167,8 +167,8 @@ export default function Home() {
           </DropdownMenu>
         </div>
       </header>
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      <main className="flex flex-1 flex-col gap-4 p-3 md:gap-8 md:p-8">
+        <div className="grid grid-cols-2 gap-4 md:gap-8 lg:grid-cols-4">
           <Card x-chunk="dashboard-01-chunk-0">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
@@ -223,152 +223,17 @@ export default function Home() {
           </Card>
         </div>
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card
-            className="xl:col-span-2" x-chunk="dashboard-01-chunk-4"
-          >
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                  <ArrowUpRight className="h-4 w-4" />
-                </Link>
-              </Button>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Customer</TableHead>
-                    <TableHead className="hidden xl:table-column">
-                      Type
-                    </TableHead>
-                    <TableHead className="hidden xl:table-column">
-                      Status
-                    </TableHead>
-                    <TableHead className="hidden xl:table-column">
-                      Date
-                    </TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Liam Johnson</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        liam@example.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Sale
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Approved
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2023-06-23
-                    </TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Olivia Smith</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        olivia@example.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Refund
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Declined
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2023-06-24
-                    </TableCell>
-                    <TableCell className="text-right">$150.00</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Noah Williams</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        noah@example.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Subscription
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Approved
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2023-06-25
-                    </TableCell>
-                    <TableCell className="text-right">$350.00</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Emma Brown</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        emma@example.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Sale
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Approved
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2023-06-26
-                    </TableCell>
-                    <TableCell className="text-right">$450.00</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Liam Johnson</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        liam@example.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Sale
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Approved
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2023-06-27
-                    </TableCell>
-                    <TableCell className="text-right">$550.00</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
+
           <Card x-chunk="dashboard-01-chunk-5">
             <CardHeader>
-              <CardTitle>Recent Sales</CardTitle>
+              <CardTitle>Transactions</CardTitle>
+              <CardDescription>
+                Recent transactions from your store.
+              </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-8">
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
+                <Avatar className="h-9 w-9">
                   <AvatarImage src="/avatars/01.png" alt="Avatar" />
                   <AvatarFallback>OM</AvatarFallback>
                 </Avatar>
@@ -380,10 +245,10 @@ export default function Home() {
                     olivia.martin@email.com
                   </p>
                 </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
+                <div className="ml-auto font-medium text-green-600">+$1,999.00</div>
               </div>
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
+                <Avatar className="h-9 w-9">
                   <AvatarImage src="/avatars/02.png" alt="Avatar" />
                   <AvatarFallback>JL</AvatarFallback>
                 </Avatar>
@@ -395,10 +260,10 @@ export default function Home() {
                     jackson.lee@email.com
                   </p>
                 </div>
-                <div className="ml-auto font-medium">+$39.00</div>
+                <div className="ml-auto font-medium text-green-600">+$39.00</div>
               </div>
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
+                <Avatar className="h-9 w-9">
                   <AvatarImage src="/avatars/03.png" alt="Avatar" />
                   <AvatarFallback>IN</AvatarFallback>
                 </Avatar>
@@ -410,10 +275,10 @@ export default function Home() {
                     isabella.nguyen@email.com
                   </p>
                 </div>
-                <div className="ml-auto font-medium">+$299.00</div>
+                <div className="ml-auto font-medium text-green-600">+$299.00</div>
               </div>
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
+                <Avatar className="h-9 w-9">
                   <AvatarImage src="/avatars/04.png" alt="Avatar" />
                   <AvatarFallback>WK</AvatarFallback>
                 </Avatar>
@@ -425,10 +290,10 @@ export default function Home() {
                     will@email.com
                   </p>
                 </div>
-                <div className="ml-auto font-medium">+$99.00</div>
+                <div className="ml-auto font-medium text-green-600">+$99.00</div>
               </div>
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
+                <Avatar className="h-9 w-9">
                   <AvatarImage src="/avatars/05.png" alt="Avatar" />
                   <AvatarFallback>SD</AvatarFallback>
                 </Avatar>
@@ -440,12 +305,11 @@ export default function Home() {
                     sofia.davis@email.com
                   </p>
                 </div>
-                <div className="ml-auto font-medium">+$39.00</div>
+                <div className="ml-auto font-medium text-green-600">+$39.00</div>
               </div>
             </CardContent>
           </Card>
         </div>
-
         <Fab />
       </main>
     </div>
--- app/views/fab.tsx
+++ app/views/fab.tsx
@@ -1,15 +1,146 @@
 import React from 'react'
 import { Button } from '../components/ui/button'
+import {
+    Credenza,
+    CredenzaBody,
+    CredenzaClose,
+    CredenzaContent,
+    CredenzaDescription,
+    CredenzaFooter,
+    CredenzaHeader,
+    CredenzaTitle,
+    CredenzaTrigger,
+} from "@/app/components/ui/credenza"
+
+import { Bird, Rabbit, Turtle } from "lucide-react"
+
+import { Input } from "@/app/components/ui/input"
+import { Label } from "@/app/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/app/components/ui/select"
+import { Textarea } from "@/app/components/ui/textarea"
 
 const Fab = () => {
     return (
-        <div className='fixed bottom-5 right-5'>
-            <Button className='rounded-2xl w-16 h-16'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="white">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                </svg>
-            </Button>
-        </div>
+        <>
+            <Credenza>
+                <CredenzaTrigger asChild>
+                    <div className='fixed bottom-5 right-5'>
+                        <Button className='rounded-2xl w-16 h-16'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="white">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                            </svg>
+                        </Button>
+                    </div>
+                </CredenzaTrigger>
+                <CredenzaContent>
+                    <CredenzaHeader>
+                        <CredenzaTitle className='text-xl leading-tight'>
+                            Add Transaction
+                        </CredenzaTitle>
+                        <CredenzaDescription>
+                            Add a new transaction to the ledger.
+                        </CredenzaDescription>
+                    </CredenzaHeader>
+                    <CredenzaBody>
+                        <div
+                            className="relative flex-col items-start gap-8"
+                        >
+                            <form className="grid w-full items-start gap-6">
+                                <fieldset className="grid gap-6 rounded-lg border p-4">
+                                    <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="model">Model</Label>
+                                        <Select>
+                                            <SelectTrigger
+                                                id="model"
+                                                className="items-start [&_[data-description]]:hidden"
+                                            >
+                                                <SelectValue placeholder="Select a model" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectItem value="genesis">
+                                                    <div className="flex items-start gap-3 text-muted-foreground">
+                                                        <Rabbit className="size-5" />
+                                                        <div className="grid gap-0.5">
+                                                            <p>
+                                                                Neural{" "}
+                                                                <span className="font-medium text-foreground">
+                                                                    Genesis
+                                                                </span>
+                                                            </p>
+                                                            <p className="text-xs" data-description>
+                                                                Our fastest model for general use cases.
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </SelectItem>
+                                                <SelectItem value="explorer">
+                                                    <div className="flex items-start gap-3 text-muted-foreground">
+                                                        <Bird className="size-5" />
+                                                        <div className="grid gap-0.5">
+                                                            <p>
+                                                                Neural{" "}
+                                                                <span className="font-medium text-foreground">
+                                                                    Explorer
+                                                                </span>
+                                                            </p>
+                                                            <p className="text-xs" data-description>
+                                                                Performance and speed for efficiency.
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </SelectItem>
+                                                <SelectItem value="quantum">
+                                                    <div className="flex items-start gap-3 text-muted-foreground">
+                                                        <Turtle className="size-5" />
+                                                        <div className="grid gap-0.5">
+                                                            <p>
+                                                                Neural{" "}
+                                                                <span className="font-medium text-foreground">
+                                                                    Quantum
+                                                                </span>
+                                                            </p>
+                                                            <p className="text-xs" data-description>
+                                                                The most powerful model for complex computations.
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="temperature">Temperature</Label>
+                                        <Input id="temperature" type="number" placeholder="0.4" />
+                                    </div>
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <div className="grid gap-3">
+                                            <Label htmlFor="top-p">Top P</Label>
+                                            <Input id="top-p" type="number" placeholder="0.7" />
+                                        </div>
+                                        <div className="grid gap-3">
+                                            <Label htmlFor="top-k">Top K</Label>
+                                            <Input id="top-k" type="number" placeholder="0.0" />
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </CredenzaBody>
+                    <CredenzaFooter>
+                        <CredenzaClose asChild>
+                            <button>Close</button>
+                        </CredenzaClose>
+                    </CredenzaFooter>
+                </CredenzaContent>
+            </Credenza>
+        </>
     )
 }
 
